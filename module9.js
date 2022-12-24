3c657ab01cb8d720f4da5b295814b381ca916442
Dennis Korir <38191858+korirdennis@users.noreply.github.com>
2022-12-24 10:12:12 +0300

    initial commit, modules learnt, While loop, do while,for loop

---
+++ module9.js
@@ -0,0 +1,49 @@
+// create a while loop to count numbers from 1-100
+
+function countTo100(){
+
+    // initialize the two variables
+    var sum=0, num=1;
+
+
+    while(num<100){
+
+        sum+=num;
+
+        num+=1
+    }
+    return sum;
+};
+
+
+
+// use do while loop
+
+function count_to_100(){
+    // initialize the two variables
+
+    var sum =0, num=1;
+
+    do{
+        sum+=num;
+        num++;
+    }while(num<100);
+
+    return sum;
+};
+
+//console.log(count_to_100());
+
+// Creating a for loop to count numbers from 1-100
+function count100(){
+
+    for(var sum=0,num=1; num<100; num++){
+
+        sum+=num;
+    }
+    return sum;
+}
+
+//console.log(`The sum is ${count100()}`); // expected output is "The sum is 4950"
+
+
